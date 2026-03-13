ce968a485853210a50efb1579b98fcf8a5474637
MrAhdyourse <mrahdyourse@users.noreply.github.com>
2026-03-13 14:24:42 +0700

    heibro

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "export",
+  basePath: "/pesantren-ramadhan-2026",
   images: {
     unoptimized: true,
   },
